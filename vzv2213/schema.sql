5b92baf2012d138057d8a0921fc8314974f40cf8
Zach Stednick <zstednic@fhcrc.org>
2013-03-06 13:51:36 -0800

    [vzv] sql spelling and tables for add_patient

--- vzv2213/schema.sql
+++ vzv2213/schema.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS demo;
 CREATE TABLE demo (upn text, uw_id text, initials text, dob date,
-                    txtype text, pre_screeing_date date,
+                    txtype text, pre_screening_date date,
                     arrival_date date, consent text, consent_reason text,
                     consent_comments text, randomize date, baseline text,
                     allocation text, txdate date, injection1 date);
