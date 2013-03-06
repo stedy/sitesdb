471c799a6593d74621d3bacf106b7273cf235ecf
Zach Stednick <zstednic@fhcrc.org>
2013-03-05 17:10:08 -0800

    [vzv] initial database config

---
+++ vzv2213/schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS demo;
+CREATE TABLE demo (upn text, uw_id text, initials text, dob date,
+                    txtype text, pre_screeing_date date,
+                    arrival_date date, consent text, consent_reason text,
+                    consent_comments text, randomize date, baseline text,
+                    allocation text, txdate date, inection1 date);
+
+
+DROP TABLE IF EXISTS checks;
+CREATE TABLE checks (amount text, checkdate date, check_no text,
+                      upn text);
