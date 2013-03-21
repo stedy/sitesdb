13c155e02b020814585cc268a587f8bd348a1e26
Zach Stednick <zstednic@fhcrc.org>
2013-03-21 16:04:36 -0700

    [vzv] remove consent comments, arrival dates

--- vzv2213/schema.sql
+++ vzv2213/schema.sql
@@ -2,23 +2,24 @@ DROP TABLE IF EXISTS demo;
 CREATE TABLE demo (upn text, uw_id text, initials text, dob date,
                     hispanic text, gender text, ethnicity text,
                     pt_userid text, txdate date,
-                    txtype text, pre_screening_date date,
-                    arrival_date date, consent text, consent_reason text,
-                    consent_comments text, randomize text, baseline text,
+                    txtype text, consent text, consent_reason text,
+                    randomize text, baseline text,
                     allocation text, txtext text, injection1 date,
                   injection2p date, injection2a date,
                   injection3p date, injection3a date,
                   injection4p date, injection4a date,
                   injection5p date, injection5a date,
                   injection6p date, injection6a date,
-                  injection7p date, injection7a date
+                  injection7p date, injection7a date,
+                  check1date date, check1amt text, check1no text,
+                  check2date date, check2amt text, check2no text,
+                  check3date date, check3amt text, check3no text,
+                  check4date date, check4amt text, check4no text,
+                  check5date date, check5amt text, check5no text,
+                  check6date date, check6amt text, check6no text,
+                  check7date date, check7amt text, check7no text
                 );
 
 
 DROP TABLE IF EXISTS user;
 CREATE TABLE user (name text, user_id text);
-
-DROP TABLE IF EXISTS checks;
-CREATE TABLE checks (amount text, checkdate date, check_no text,
-                      upn text);
-
