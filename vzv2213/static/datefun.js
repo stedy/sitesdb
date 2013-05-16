8c6b43de0bfaa531d0113e18ca5868ab0b388c95
Zach Stednick <zstednic@fhcrc.org>
2013-05-16 15:25:23 -0700

    [vzv] fixed call date jquery

--- vzv2213/static/datefun.js
+++ vzv2213/static/datefun.js
@@ -1,16 +1,13 @@
 $(function()
-{
-	$('td.date').each(function()
-	{
-		var row_date = Date.parse($(this).text().replace(/(\d{2})\/(\d{2})\/(\d{4})/, '$2/$1/$3'));
-		var now_date = new Date().getTime();
-		if(row_date > now_date)
-		{
-			$(this).addClass('past');
-		}
-	}
-	);
-}
-);
-
-
+    {
+          $('#dates_table .date').each(function() {
+                    var phonecall = Date.parse($(this).text().replace(/(\d{2})\/(\d{2})\/(\d{4})/, '$2/$1/$3'));
+                    var now_date = new Date().getTime();
+                    if(phonecall >= now_date + 1209600000)
+                    {
+                    $(this).addClass('call');
+                                          }
+              }
+                  );
+    }
+ );
