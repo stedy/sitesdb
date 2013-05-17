b8dd1246db2fc9250b4a5608e6a5d20557d950b0
Zach Stednick <zstednic@fhcrc.org>
2013-05-17 10:46:13 -0700

    [vzv] date highlighting finally working

--- vzv2213/static/datefun.js
+++ vzv2213/static/datefun.js
@@ -1,9 +1,10 @@
 $(function()
     {
           $('#dates_table .date').each(function() {
-                    var phonecall = Date.parse($(this).text().replace(/(\d{2})\/(\d{2})\/(\d{4})/, '$2/$1/$3'));
+                    var phonecall = Date.parse($(this).text().replace(/(\d{2})\/(\d{2})\/(\d{4})/, '$1/$2/$3'));
                     var now_date = new Date().getTime();
-                    if(phonecall >= now_date + 1209600000)
+                    
+                    if(phonecall <= now_date + 1210000000 && phonecall > now_date)
                     {
                     $(this).addClass('call');
                                           }
