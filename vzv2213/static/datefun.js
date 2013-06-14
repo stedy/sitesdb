27c45181fbc88b140499422ff2788be005c7f7a2
Zach Stednick <zstednic@fhcrc.org>
2013-06-14 12:50:49 -0700

    [vzv] fixed datefunction js

--- vzv2213/static/datefun.js
+++ vzv2213/static/datefun.js
@@ -1,7 +1,7 @@
 $(function()
     {
           $('#dates_table .date').each(function() {
-                    var phonecall = Date.parse($(this).text().replace(/(\d{2})-(\d{2})-(\d{4})/, '$2/$1/$3'));
+                    var phonecall = Date.parse($(this).text().replace(/(\d{2})\/(\d{2})\/(\d{4})/, '$1/$2/$3'));
                     var now_date = new Date().getTime();
                     
                     if(phonecall <= now_date + 1210000000 && phonecall > now_date)
