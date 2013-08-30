fc416fc56e07b97327a562a6c08e54cf3dbb7a95
Zach Stednick <zstednic@fhcrc.org>
2013-08-30 16:14:30 -0700

    [vzvdb] now adding color syntax

--- vzv2213/static/datefun.js
+++ vzv2213/static/datefun.js
@@ -3,11 +3,14 @@ $(function()
           $('#dates_table .date').each(function() {
                     var phonecall = Date.parse($(this).text().replace(/(\d{2})\/(\d{2})\/(\d{4})/, '$1/$2/$3'));
                     var now_date = new Date().getTime();
-                    
                     if(phonecall <= now_date + 1210000000 && phonecall > now_date)
                     {
                     $(this).addClass('call');
                                           }
+                    if(phonecall > now_date - 1210000000 && phonecall < now_date)
+                    {
+                      $(this).addClass('past');
+                    }
               }
                   );
     }
