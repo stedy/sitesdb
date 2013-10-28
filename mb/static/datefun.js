116968d7fe24a23f87090fa98cb152a04016a118
Zach Stednick <zstednic@fhcrc.org>
2013-10-28 15:23:37 -0700

    [mb] cleanup mb before re-initating

---
+++ mb/static/datefun.js
@@ -0,0 +1,16 @@
+$(function()
+{
+	$('td.date').each(function()
+	{
+		var row_date = Date.parse($(this).text().replace(/(\d{2})\/(\d{2})\/(\d{4})/, '$2/$1/$3'));
+		var now_date = new Date().getTime();
+		if(row_date > now_date)
+		{
+			$(this).addClass('past');
+		}
+	}
+	);
+}
+);
+
+
