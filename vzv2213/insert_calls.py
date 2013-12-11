4d2ef976500e1be2dba7c430c4bf9d402d35b497
stedy <zstednic@fhcrc.org>
2013-12-10 16:05:46 -0800

    remove headers for dumping CSV

--- vzv2213/insert_calls.py
+++ vzv2213/insert_calls.py
@@ -7,16 +7,14 @@ cursor = connection.cursor()
 
 cursor.execute('DROP TABLE IF EXISTS calls')
 cursor.execute("""CREATE TABLE calls (allocation text, expected_calldate date, initials text,
-			show_calldate text,
-                    expected_calldate_sql date, calltype text, actual_calldate
-                    text, actual_calldate_sql date, phonenumber text,
+			show_calldate text, expected_calldate_sql date, calltype text, 
+			actual_calldate text, actual_calldate_sql date, phonenumber text,
                     call_check_no text, call_check_amt text, email text)""")
 connection.commit()
 
-csvre = csv.reader(open("calls.csv", 'rb'), delimiter = ",", quotechar='"')
+csvre = csv.reader(open("dumpedcalls.csv", 'rb'), delimiter = ",", quotechar='"')
 
 t = (csvre,)
-csvre.next()
 for t in csvre:
 	cursor.execute('INSERT into calls VALUES (?,?,?,?,?,?,?,?,?,?,?,?)', t)
 connection.commit()
