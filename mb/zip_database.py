5a6d7bffc3ef2b2dc7afeaad54886293319463a3
Zach Stednick <zstednic@fhcrc.org>
2014-01-06 17:21:17 -0800

    [mb] now zipping up tables from database

--- mb/zip_database.py
+++ mb/zip_database.py
@@ -1,10 +1,24 @@
 import zipfile
 import glob
+import datetime as dt
+import sqlite3
+import csv
+
+with sqlite3.connect('mb.db') as connection:
+    csvWriter = csv.writer(open("demo.csv", "w"))
+    c = connection.cursor()
+    code = c.execute("SELECT * from demo")
+    rows = code.fetchall()
+    headers = []
+    for colinfo in c.description:
+        headers.append(colinfo[0])
+    csvWriter.writerow(headers)
+    csvWriter.writerows(rows)
 
 files = glob.glob('*.csv')
-z = zipfile.ZipFile('myzip.zip', 'w')
+now = dt.datetime.now().strftime('%Y-%m-%d')
+z = zipfile.ZipFile(now + "_database.zip", 'w')
 
 for x in files:
     z.write(x)
-    print x
 z.close()
