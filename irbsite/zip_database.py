e70c66d0f82630493e6818519c88e75de090daba
root <root@hooligan.fhcrc.org>
2014-04-23 10:21:18 -0700

    change chmod on zip db

--- irbsite/zip_database.py
+++ irbsite/zip_database.py
@@ -1,13 +1,13 @@
 import zipfile
-import glob
 import datetime as dt
 import sqlite3
 import csv
 import os
+import stat
 
 def main():
-    with sqlite3.connect('irb_db.db') as connection:
-        csvWriter = csv.writer(open("archives/safety_reports.csv", "w"))
+    with sqlite3.connect('/var/www/html/irbdb/irb_db.db') as connection:
+        csvWriter = csv.writer(open("/var/www/html/irbdb/archives/safety_reports.csv", "w"))
         c = connection.cursor()
         code = c.execute("SELECT * from safety")
         rows = code.fetchall()
@@ -17,13 +17,13 @@ def main():
         csvWriter.writerow(headers_demo)
         csvWriter.writerows(rows)
 
-    files = glob.glob('*.csv')
     now = dt.datetime.now().strftime('%Y-%m-%d')
     z = zipfile.ZipFile(os.path.join('archives', now + "_database.zip"), 'w')
 
-    for x in files:
-        z.write(os.path.join('archives', x))
+
+    z.write("/var/www/html/irbdb/archives/safety_reports.csv")
     z.close()
+    os.chmod(os.path.join('archives', now + "_database.zip"), 0777)
 
 if __name__ == "__main__":
     main()
