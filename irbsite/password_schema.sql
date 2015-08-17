c114da559ab921f968d569159d93b515a614c517
stedy <zstednic@fhcrc.org>
2015-08-17 16:06:54 -0700

    separate password schema to avoid accidentally overwriting

---
+++ irbsite/password_schema.sql
@@ -0,0 +1,3 @@
+DROP TABLE IF EXISTS user;
+CREATE TABLE user (user_id integer primary key autoincrement, username string,
+                     email string, pw_hash string);
--- irbsite/schema.sql
+++ irbsite/schema.sql
@@ -1,7 +1,3 @@
-DROP TABLE IF EXISTS user;
-CREATE TABLE user (user_id integer primary key autoincrement, username string,
-                     email string, pw_hash string);
-
 DROP TABLE IF EXISTS dontype;
 CREATE TABLE dontype (
     Protocol text,
