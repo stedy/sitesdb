ab6998759c434448b243a23be3413d2c9f81d777
stedy <zstednic@fhcrc.org>
2015-08-24 11:26:21 -0700

    now zipping up all tables from database

--- irbsite/zip_database.py
+++ irbsite/zip_database.py
@@ -7,27 +7,26 @@ from glob import glob
 
 def main():
     with sqlite3.connect('irb_site.db') as connection:
-        with open("archives/protocols.csv", "w") as cw:
-            csvWriter = csv.writer(cw)
-            c = connection.cursor()
-            code = c.execute("SELECT * from protocols")
-            rows = code.fetchall()
-            headers_demo = []
-            for colinfo in c.description:
-                headers_demo.append(colinfo[0])
-            csvWriter.writerow(headers_demo)
-            csvWriter.writerows(rows)
-
-        with open("archives/reviewcomm.csv", "w") as rc:
-            csvWriter = csv.writer(rc)
-            c = connection.cursor()
-            code = c.execute("SELECT * from reviewcomm")
-            rows = code.fetchall()
-            headers_demo = []
-            for colinfo in c.description:
-                headers_demo.append(colinfo[0])
-            csvWriter.writerow(headers_demo)
-            csvWriter.writerows(rows)
+        tables = []
+        c = connection.cursor()
+        c.execute("""SELECT name FROM sqlite_master WHERE type = 'table';""")
+        for x in c.fetchall():
+            tables.append(x[0])
+        tables.remove('user')
+        tables.remove('sqlite_sequence')
+        for table in tables:
+            fn = "archives/" + table + ".csv"
+            with open(fn, "w") as outfile:
+                csvWriter = csv.writer(outfile)
+                c = connection.cursor()
+                cmd = "SELECT * FROM " + table
+                code = c.execute(cmd)
+                rows = code.fetchall()
+                headers_demo = []
+                for colinfo in c.description:
+                    headers_demo.append(colinfo[0])
+                csvWriter.writerow(headers_demo)
+                csvWriter.writerows(rows)
 
     now = dt.datetime.now().strftime('%Y-%m-%d')
     with zipfile.ZipFile(os.path.join('archives', now + "_database.zip"), 'w') as zf:
